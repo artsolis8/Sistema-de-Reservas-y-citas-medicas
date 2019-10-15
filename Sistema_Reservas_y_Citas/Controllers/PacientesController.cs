6100b9fb317013a38136ec0674fcd31cd37c629b
artsolis8 <artsolis8@gmail.com>
2019-10-15 19:38:25 -0300

    plantilla css cambiada y modelo añadido

---
+++ Sistema_Reservas_y_Citas/Controllers/PacientesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Sistema_Reservas_y_Citas.Models;
+
+namespace Sistema_Reservas_y_Citas.Controllers
+{
+    public class PacientesController : ApiController
+    {
+        private ConsultaMedicaEntities db = new ConsultaMedicaEntities();
+
+        // GET: api/Pacientes
+        public IQueryable<Paciente> GetPaciente()
+        {
+            return db.Paciente;
+        }
+
+        // GET: api/Pacientes/5
+        [ResponseType(typeof(Paciente))]
+        public async Task<IHttpActionResult> GetPaciente(int id)
+        {
+            Paciente paciente = await db.Paciente.FindAsync(id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(paciente);
+        }
+
+        // PUT: api/Pacientes/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPaciente(int id, Paciente paciente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != paciente.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(paciente).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PacienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Pacientes
+        [ResponseType(typeof(Paciente))]
+        public async Task<IHttpActionResult> PostPaciente(Paciente paciente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Paciente.Add(paciente);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = paciente.id }, paciente);
+        }
+
+        // DELETE: api/Pacientes/5
+        [ResponseType(typeof(Paciente))]
+        public async Task<IHttpActionResult> DeletePaciente(int id)
+        {
+            Paciente paciente = await db.Paciente.FindAsync(id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            db.Paciente.Remove(paciente);
+            await db.SaveChangesAsync();
+
+            return Ok(paciente);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PacienteExists(int id)
+        {
+            return db.Paciente.Count(e => e.id == id) > 0;
+        }
+    }
+}
