3fb7ffc69a38967cdb28ba8d9812db81b1a18291
adriansoa <asoa17@hotmail.com>
2019-10-17 19:02:28 -0300

    Consulta/Reporte de Reservas con autentificación

---
+++ Sistema_Reservas_y_Citas/Controllers/ReservasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Sistema_Reservas_y_Citas.Models;
+
+namespace Sistema_Reservas_y_Citas.Controllers
+{
+    public class ReservasController : Controller
+    {
+        private ConsultaMedicaEntities db = new ConsultaMedicaEntities();
+        [Authorize]
+
+        // GET: Reservas
+        public ActionResult Index()
+        {
+            return View(db.Reserva.ToList());
+        }
+
+        // GET: Reservas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reserva reserva = db.Reserva.Find(id);
+            if (reserva == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reserva);
+        }
+
+        // GET: Reservas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reservas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,paciente,medico,funcionario,fecha_registro,fecha_reservada,estados,monto")] Reserva reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reserva.Add(reserva);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(reserva);
+        }
+
+        // GET: Reservas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reserva reserva = db.Reserva.Find(id);
+            if (reserva == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reserva);
+        }
+
+        // POST: Reservas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,paciente,medico,funcionario,fecha_registro,fecha_reservada,estados,monto")] Reserva reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reserva).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(reserva);
+        }
+
+        // GET: Reservas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reserva reserva = db.Reserva.Find(id);
+            if (reserva == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reserva);
+        }
+
+        // POST: Reservas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reserva reserva = db.Reserva.Find(id);
+            db.Reserva.Remove(reserva);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
